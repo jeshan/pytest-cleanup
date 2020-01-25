35518b900871251029d9a16f5f5fd19b8a3ac1eb
jeshan <j@jeshan.co>
2020-01-26 01:55:39 +0400

    version 0.3: more docs

--- pytest_cleanup/__init__.py
+++ pytest_cleanup/__init__.py
@@ -1,6 +1,6 @@
 """Automated, comprehensive and well-organised pytest test cases."""
 
-__version__ = '0.2'
+__version__ = '0.3'
 
 from .recorder import Recorder, get_test_data_filename, user_function
 from .runtime import parametrize_stg_tests
