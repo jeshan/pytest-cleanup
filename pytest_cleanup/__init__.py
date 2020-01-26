e26878f528a9c4f58fdafe326b466d1dd6701cba
jeshan <j@jeshan.co>
2020-01-26 15:27:12 +0400

    match PEP 440

--- pytest_cleanup/__init__.py
+++ pytest_cleanup/__init__.py
@@ -5,6 +5,6 @@ import os
 from .recorder import Recorder, get_test_data_filename, user_function
 from .runtime import parametrize_stg_tests
 
-__version__ = os.environ.get('VERSION', 'local')
+__version__ = os.environ.get('VERSION', '0')
 
 __all__ = ['parametrize_stg_tests', 'Recorder', 'get_test_data_filename', 'user_function']
